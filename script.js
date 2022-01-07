6e82c271e0d33509ec3319dfc7b12237cb074872
SiddhantG11 <siddhantgupta0511@gmail.com>
2022-01-07 12:06:17 +0530

    added one round functions, adding only loop and result logging remains

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Write function to return rock paper or scissors 
 
-// Test it 
 
 // Write second function that plays one round of RPS 
     //make it case insensitive 
@@ -10,12 +9,39 @@
     //loop over 5 times 
     //use prompt to get input from user 
 
-const gameChoices = ["Rock","Paper","Scissors"];
+const gameChoices = ["rock","paper","scissors"];
 
 function computerPlay() {
     return gameChoices[Math.floor(Math.random()*gameChoices.length)];
 }
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+
+
+function playRound (userInput, computerSelection) {
+    
+    let userInput = prompt('Please enter rock paper or scissors');
+    let newInput = userInput.toLowerCase();
+    if (newInput == computerSelection) {
+        return 'tie game';
+    }
+    else if ((newInput == 'rock' && computerSelection == 'scissors') || (newInput == 'paper' && computerSelection == 'rock') || (newInput == 'scissors' && computerSelection == 'paper'))
+     {
+        return 'you win!';
+    }
+    else {
+        return 'you loose';
+    }
+
+}
+
+
+function game() {
+    for(i = 0; i<6; i++) {
+        const computerSelection = computerPlay();
+        playRound(userInput,computerSelection);
+        console.log(playRound(userInput, computerSelection));
+    }
+    
+}
+game();
+
